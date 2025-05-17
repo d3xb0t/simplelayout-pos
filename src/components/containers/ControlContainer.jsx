e4b4ffd4a1c07d868ace3e55229ceddc64474a23
d3xb0t <d3xb0t@gmail.com>
2025-05-17 18:04:51 -0500

    Agregados ErrorBoundary y suspense

--- src/components/containers/ControlContainer.jsx
+++ src/components/containers/ControlContainer.jsx
@@ -1,17 +1,21 @@
 /**
  * [Core]
  */
-import React, { useState } from 'react'
+import React, { lazy, useState, memo, Suspense } from 'react';
+import CircularProgress from '@mui/material/CircularProgress'
+import { ErrorBoundary } from 'react-error-boundary';
 
 /**
  * [Assets]
  */
 import styles from '../../assets/styles/AppStyles.module.css'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import ListIcon from '@mui/icons-material/List'
-import IconButton from '@mui/material/IconButton'
 import Badge, { badgeClasses } from '@mui/material/Badge'
 import { styled } from '@mui/material/styles'
+
+const ShoppingCartIcon = lazy(() => import('@mui/icons-material/ShoppingCart'))
+const ListIcon = lazy(() => import('@mui/icons-material/List'))
+const IconButton = lazy(() => import('@mui/material/IconButton'))
+
 
 const CartBadge = styled(Badge)`
     & .${badgeClasses.badge} {
@@ -24,15 +28,27 @@ const ControlContainer = ({ setNav }) => {
     const [cartItems, setCartItems] = useState(2)
     return (
         <div className={styles.controls}>
-            <IconButton className={styles.controlButton} aria-label="List view"
-                onClick={() => setNav(prev => !prev)}
+            <ErrorBoundary
+                fallback={<div>⚠️ An error has occurred</div>}
             >
-                <ListIcon fontSize='small' />
-            </IconButton>
-            <IconButton className={styles.controlButton} aria-label="Shopping cart">
-                <ShoppingCartIcon fontSize="small" />
-                <CartBadge badgeContent={2} color="primary" overlap="circular" />
-            </IconButton>
+                <Suspense fallback={<CircularProgress size={20} />}>
+                    <IconButton className={styles.controlButton} aria-label="List view"
+                        onClick={() => setNav(prev => !prev)}
+                    >
+                        <ListIcon fontSize='small' />
+                    </IconButton>
+                </Suspense>
+            </ErrorBoundary>
+            <ErrorBoundary
+                fallback={<div>⚠️ An error has occurred</div>}
+            >
+                <Suspense fallback={<CircularProgress size={20} />}>
+                    <IconButton className={styles.controlButton} aria-label="Shopping cart">
+                        <ShoppingCartIcon fontSize="small" />
+                        <CartBadge badgeContent={2} color="primary" overlap="circular" />
+                    </IconButton>
+                </Suspense>
+            </ErrorBoundary>
         </div>
     )
 }
